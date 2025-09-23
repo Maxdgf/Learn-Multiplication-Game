257a4d8b4b08ba6d74cb04bede7d00e61eaa3377
Max <162334963+Maxdgf@users.noreply.github.com>
2025-09-23 22:29:46 +0300

    Update ObjectSpawner.cs

--- Assets/Scripts/Modules/ObjectSpawner.cs
+++ Assets/Scripts/Modules/ObjectSpawner.cs
@@ -1,11 +1,17 @@
+/*
+==================
+| Object spawner |
+==================
+*/
+
 using System.Collections;
 using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
 {
-    [SerializeField] private float spawnDuration;
-    [SerializeField] private int marginY;
-    [SerializeField] private GameObject objectToSpawn;
+    [SerializeField] private float spawnDuration; //spawn delay
+    [SerializeField] private int marginY; //y bottom margin (to prevent objects from appearing under the UI)
+    [SerializeField] private GameObject objectToSpawn; //object to spawn
 
     private System.Random random;
     private float screenHeight;
@@ -13,29 +19,30 @@ public class ObjectSpawner : MonoBehaviour
 
     void Start()
     {
-        Camera camera = Camera.main;
+        Camera camera = Camera.main; //getting camera
 
-        random = new System.Random();
-        screenHeight = camera.orthographicSize;
-        screenWidth = screenHeight * camera.aspect;
+        random = new System.Random(); //init random
+        screenHeight = camera.orthographicSize; //getting screen height (for calculation spawn pos in device screen area)
+        screenWidth = screenHeight * camera.aspect; //getting screen width (for calculation spawn pos in device screen area)
 
-        StartCoroutine(SpawnObject(spawnDuration));
+        StartCoroutine(SpawnObject(spawnDuration)); //start infinity object spawn coroutine
     }
 
+    //object spawn infinity coroutine
     private IEnumerator SpawnObject(float spawnDuration)
     {
         while (true)
         {
-            int width = (int) screenWidth;
-            int height = (int) screenHeight;
+            int width = (int) screenWidth; //screen width - integer
+            int height = (int) screenHeight; //screen height - integer
 
-            int rndY = random.Next(0 + marginY, height);
+            int rndY = random.Next(0 + marginY, height); //random y in range(y bottom margin - screen height)
 
-            Vector2 spawnPos = new Vector2(width + width, rndY);
+            Vector2 spawnPos = new Vector2(width + width, rndY); //configuring spawn pos
 
-            Instantiate(objectToSpawn, spawnPos, Quaternion.identity);
+            Instantiate(objectToSpawn, spawnPos, Quaternion.identity); //spawn object
 
-            yield return new WaitForSeconds(spawnDuration);
+            yield return new WaitForSeconds(spawnDuration); //delay
         }
     }
 }
